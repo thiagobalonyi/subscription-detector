73086f2786efd01688d090bd7fac6c175ee5496a
thiagobalonyi <thiagocandal@gmail.com>
2026-04-06 22:33:11 -0300

    feat: implementar input, estado e lógica inicial de detecção de assinaturas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
 function App() {
+  const [texto, setTexto] = useState("");
+  const [resultado, setResultado] = useState("");
+  const analisarTexto = () => {
+    if (texto.includes("Netflix")) {
+      setResultado("Encontramos Netflix");
+    } else {
+      setResultado("Nenhuma Assinatura Encontrada");
+    }
+  };
+
   return (
     <div style={{ padding: 20 }}>
-      <h1>Detector de Assinaturas</h1>
-      <p>Projeto iniciado</p>
+      <h1>Teste useState</h1>
+
+      <input
+      value={texto}
+      onChange={(e) => setTexto(e.target.value)}
+      />
+
+    <button onClick={analisarTexto}>
+      Analisar
+    </button>
+
+      <p>{texto}</p>
+      <p>{resultado}</p>
     </div>
   );
 }
